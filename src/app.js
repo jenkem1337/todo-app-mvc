8c68b91dbaeccc1b5138425aae96295f2b270188
Can <yamahasirossi@gmail.com>
2022-06-30 02:41:46 +0300

    event handlers injected to todo model

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
 import TodoController from "./controller/TodoController.js";
+import TodoCompletedStateChangedEventHandler from "./model/event/EventHandler/TodoCompletedStateChangedEventHandler.js";
+import TodoCreatedEventHandler from "./model/event/EventHandler/TodoCreatedEventHandler.js";
+import TodoDeletedEventHandler from "./model/event/EventHandler/TodoDeletedEvent.js";
+import TodoTextUpdatedEventHandler from "./model/event/EventHandler/TodoTextUpdatedEventHandler.js";
+import TodoCompletedStatedChangedEvent from "./model/event/TodoCompletedStatedChangedEvent.js";
+import TodoCreatedEvent from "./model/event/TodoCreatedEvent.js";
+import TodoDeletedEvent from "./model/event/TodoDeletedEvent.js";
+import TodoTextUpdatedEvent from "./model/event/TodoTextUpdatedEvent.js";
 import Todo from "./model/Todo.js";
 import TodoCollection from "./model/TodoCollection.js";
 import TodoView from "./view/TodoView.js";
 
-let todoModel = new Todo()
+let todoModel = new Todo(new Map([
+    [TodoCreatedEvent.name, new TodoCreatedEventHandler()],
+    [TodoDeletedEvent.name, new TodoDeletedEventHandler()],
+    [TodoTextUpdatedEvent.name, new TodoTextUpdatedEventHandler()],
+    [TodoCompletedStatedChangedEvent.name, new TodoCompletedStateChangedEventHandler()]
+]))
 let todoCollection = new TodoCollection()
 let todoController = new TodoController(todoModel,todoCollection)
 let todoView = new TodoView(todoController)
