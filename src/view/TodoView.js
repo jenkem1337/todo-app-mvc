144c05f45100a79359910c4862d975447a3344f1
Can <yamahasirossi@gmail.com>
2022-06-30 12:26:45 +0300

    more logic added

--- src/view/TodoView.js
+++ src/view/TodoView.js
@@ -2,14 +2,19 @@ import ObserverView from "./ObserverView.js";
 
 export default class TodoView extends ObserverView {
     #todoController
-    #todos = new Array()
+    #state
     constructor(todoController){
         super()
         this.#todoController = todoController
+        this.#state = {
+            todos: new Array(),
+            hello: ''
+        }
+        this.render()
     }
     update(fromCollection){
-        this.#todos = fromCollection
-        this.#todos.reverse()
+        this.#state.todos = fromCollection
+        this.#state.todos.reverse()
         this.render()
     }
     
@@ -70,18 +75,33 @@ export default class TodoView extends ObserverView {
     render(){
         let view =   `
         <h1>Todo App</h1>
-        <h3 style="color:red;" id="error-msg"></h3> 
-        <input id="todo-text-input" type="text" placeholder="add new todo">
+        <h3 style="color:red;" id="error-msg" >${this.#state.hello}</h3>
+        <h3>Todo Length ${this.#state.todos.length}</h3>
+        <input id="todo-text-input" type="text" placeholder="add new todo or update">
         <button onclick="todoView.createNewTodo(event)">Add New Todo</button>
-        ${this.#todos.map(todo => {
-            return `<p>Todo Id      => ${todo.getId()}</p>
-               <p>Todo Text    => ${todo.getTodo()}</p>
-               <p>Is Completed => ${todo.getIsCompleted()}</p>
-               <p>Created At   => ${todo.getCreatedAt()}</p>
-               <p>Updated At   => ${todo.getUpdatedAt()}</p>
-               <button onclick="todoView.updateTodoText(${todo.getId()}, event)">Update Todo</button>
-               <button onclick="todoView.updateCompletedState(${todo.getId()}, event)">Update Complated State</button>
-               <button onclick="todoView.deleteTodo(${todo.getId()}, event)">Delete Todo</button>`}).join('')}
+        ${this.#state.todos.map(todo => {
+            if(!todo.getIsCompletedBool()){
+                return `<p>Todo Id      => ${todo.getId()}</p>
+                <p>Todo Text    => ${todo.getTodo()}</p>
+                <p>Is Completed => ${todo.getIsCompleted()}</p>
+                <p>Created At   => ${todo.getCreatedAt()}</p>
+                <p>Updated At   => ${todo.getUpdatedAt()}</p>
+                <button onclick="todoView.updateTodoText(${todo.getId()}, event)">Update Todo</button>
+                <button onclick="todoView.updateCompletedState(${todo.getId()}, event)">Update Complated State</button>
+                <button onclick="todoView.deleteTodo(${todo.getId()}, event)">Delete Todo</button>`
+            }else {
+                return `<p>Todo Id      => ${todo.getId()}</p>
+                <p>Todo Text    => ${todo.getTodo()}</p>
+                <p>Is Completed => ${todo.getIsCompleted()}</p>
+                <p>Created At   => ${todo.getCreatedAt()}</p>
+                <p>Updated At   => ${todo.getUpdatedAt()}</p>
+                <button onclick="todoView.updateTodoText(${todo.getId()}, event)" disabled>Update Todo</button>
+                <button onclick="todoView.updateCompletedState(${todo.getId()}, event)" >Update Complated State</button>
+                <button onclick="todoView.deleteTodo(${todo.getId()}, event)">Delete Todo</button>`
+
+            }
+            })
+        }
         `
         document.getElementById('root').innerHTML = view 
 
