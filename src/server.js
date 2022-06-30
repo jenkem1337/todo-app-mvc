db8ac9754b5bf6e55df5ff044af3ad768b081cd9
Can <yamahasirossi@gmail.com>
2022-06-30 19:41:59 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 let app = express()
 app.use("/src", express.static(path.join(__dirname)))
-app.get('/todo' , (req, res) => {
+app.get('*' , (req, res) => {
     res.sendFile(path.join(__dirname,"/view/index.html"));
 })
 
