3a17f24bf1a7efac2dfae5396d72878d8b347da2
Can <yamahasirossi@gmail.com>
2022-09-10 18:39:49 +0300

    deleted unnecessary codes

--- src/app.js
+++ src/app.js
@@ -1,26 +1,10 @@
 import TodoController from "./controller/TodoController.js";
-import TodoCompletedStateChangedEventHandler from "./model/event/EventHandler/TodoCompletedStateChangedEventHandler.js";
-import TodoCreatedEventHandler from "./model/event/EventHandler/TodoCreatedEventHandler.js";
-import TodoDeletedEventHandler from "./model/event/EventHandler/TodoDeletedEvent.js";
-import TodoTextUpdatedEventHandler from "./model/event/EventHandler/TodoTextUpdatedEventHandler.js";
-import TodoCompletedStatedChangedEvent from "./model/event/TodoCompletedStatedChangedEvent.js";
-import TodoCreatedEvent from "./model/event/TodoCreatedEvent.js";
-import TodoDeletedEvent from "./model/event/TodoDeletedEvent.js";
-import TodoTextUpdatedEvent from "./model/event/TodoTextUpdatedEvent.js";
-import Todo from "./model/Todo.js";
-import TodoCollection from "./model/TodoCollection.js";
 import TodoView from "./view/TodoView.js";
-
-let todoModel = new Todo(new Map([
-    [TodoCreatedEvent.name, new TodoCreatedEventHandler()],
-    [TodoDeletedEvent.name, new TodoDeletedEventHandler()],
-    [TodoTextUpdatedEvent.name, new TodoTextUpdatedEventHandler()],
-    [TodoCompletedStatedChangedEvent.name, new TodoCompletedStateChangedEventHandler()]
-]))
-let todoCollection = new TodoCollection()
-let todoController = new TodoController(todoModel,todoCollection)
+import TodoAggregate from './model/TodoAggregate.js'
+let todoAggregate = new TodoAggregate()
+let todoController = new TodoController(todoAggregate)
 let todoView = new TodoView(todoController)
-todoCollection.addObserver(todoView)
+todoAggregate.addObserver(todoView)
 
 window.todoView = todoView
 
